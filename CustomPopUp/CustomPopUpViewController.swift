c01da32dc41390cd81b1407067295afe01108948
saranghe41 <saranghe-0-@hanmail.net>
2021-10-29 17:12:29 +0900

    기본 webView url 네이버주소 설정, subscribBtn을 githubBtn로 변경하여 github주소 연결

---
+++ CustomPopUp/CustomPopUpViewController.swift
@@ -0,0 +1,49 @@
+//
+//  PopUp.swift
+//  CustomPopUp
+//
+//  Created by 김지은 on 2021/10/29.
+//
+
+import Foundation
+import UIKit
+
+class CustomPopUpViewController: UIViewController {
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var subscribBtn: UIButton!
+    @IBOutlet weak var bgBtn: UIButton!
+    @IBOutlet weak var myImg: UIImageView!
+    
+    var subscribBtnCompletionClosure: (() -> Void)? // ()-> Void: 아무행위도 하지않는다
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("CustomPopUpViewController - viewDidLoad() called")
+        
+        contentView.layer.cornerRadius = 30
+        subscribBtn.layer.cornerRadius = 10
+        myImg.layer.cornerRadius = 40
+    }
+    
+    @IBAction func onBgBtnClicked(_ sender: Any) {
+        print("CustomPopUpViewController - onBgBtnClicked() called")
+        
+        // 현재창 닫기
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onSubScribBtnClicked(_ sender: Any) {
+        print("CustomPopUpViewController - onSubScribBtnClicked() called")
+        
+        // 현재창 닫기
+        self.dismiss(animated: true, completion: nil)
+        
+        // 컴플레션 블럭 호출
+        if let subscribBtnCompletionClosure = subscribBtnCompletionClosure {
+            // 메인에 알림
+            subscribBtnCompletionClosure()
+        }
+    }
+}
--- CustomPopUp/ViewController.swift
+++ CustomPopUp/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var popupBtn: UIButton!
+    @IBOutlet weak var myWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let bagicURL = URL(string: "http://www.naver.com")
+        myWebView.load(URLRequest(url: bagicURL!))
     }
-
-
+    
+    @IBAction func onPopupBtnClicked(_ sender: Any) {
+        print("ViewController - onPopupBtnClicked() called")
+        
+        // 스토리보드 가져오기
+        let storyboard = UIStoryboard.init(name: "PopUp", bundle: nil)
+        
+        // 스토리보드를 통해 뷰컨트롤러 가져오기
+        let customPopUpVC = storyboard.instantiateViewController(withIdentifier: "AlertPopUpVC") as! CustomPopUpViewController
+        
+        // 뷰컨트롤러가 보여지는 스타일
+        customPopUpVC.modalPresentationStyle = .overCurrentContext
+        
+        // 뷰컨트롤러가 사라지는 스타일
+        customPopUpVC.modalTransitionStyle = .crossDissolve // 스르르 사라지는 스타일
+        
+        customPopUpVC.subscribBtnCompletionClosure = {
+            print("컴플레션 블럭이 호출되었다.")
+            
+            let myChannelUrl = URL(string: "https://github.com/saranghe41")
+            self.myWebView.load(URLRequest(url: myChannelUrl!))
+        }
+        
+        // present : 현재 viewcontroller에서 다른 viewcontroller를 보여주는 기능
+        self.present(customPopUpVC, animated: true, completion: nil) // comletion: 보여주고나서 처리할것인가에 대한 설정
+    }
 }
 
